ee85067ac7a3a70bca7c14d342fc507666b74443
2912bruno <admin@soalhe.com.br>
2025-06-11 18:25:43 -0300

    Primeiro commit do dashboard

---
+++ src/main.jsx
@@ -0,0 +1,1 @@
+import React from 'react'; import ReactDOM from 'react-dom/client'; import Dashboard from './Dashboard.jsx'; ReactDOM.createRoot(document.getElementById('root')).render(<Dashboard />);
